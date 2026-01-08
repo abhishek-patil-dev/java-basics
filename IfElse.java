98c6e311ed714f5c452c2a2b70490f376f8dbbc7
abhishek-patil-dev <taurabhishek07@gmail.com>
2026-01-08 21:53:04 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,11 @@
+class IfElse{
+  public static void main(String [] args){
+    int marks = 65;
+    if(marks>=35){
+      System.out.println("Result: Pass");
+    }
+    else{
+      System.out.println("Result: Fail");
+    }
+  }
+}
