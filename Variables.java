d4b621fa04950a25ac7295228852d5f4a4b9d7c4
abhishek-patil-dev <taurabhishek07@gmail.com>
2026-01-08 21:45:31 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,9 @@
+class Variables{
+  public static void main(String[] args){
+    int age = 18;
+    String name = "Abhishek Patil";
+
+  System.out.println("Name: " + name);
+  System.out.println("Age: " + age);
+  }
+}
