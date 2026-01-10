86fba1fb2254ba0dbec308715f4445a490a25e1b
abhishek-patil-dev <taurabhishek07@gmail.com>
2026-01-10 22:27:47 +0530

    Create FactorialNumberWhile.java

---
+++ FactorialNumberWhile.java
@@ -0,0 +1,11 @@
+class FactorialNumberWhile{
+  public static void main(String[] args){
+    int fact = 1;
+    int i = 1;
+    while(i <= 5){
+      fact = fact * i;
+      i++;
+        }
+    System.out.println(fact);
+  }
+}
